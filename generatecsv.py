c407fdce8a056fe0bef5bec95819c74a49d12ae3
kokhouser <hzk11a@acu.edu>
2014-10-30 10:06:47 -0500

    Added generate file to generate test data. Added extra values.

--- generatecsv.py
+++ generatecsv.py
@@ -35,48 +35,23 @@ def get_full_name(gender=None):
     return u"%s %s" % (get_first_name(gender), get_last_name())
 
 #Code for discombobulation
-name_input = []
-banner_input = []
-#bd_input = []
-#class_input = []
-f = open('newcsv2.csv', 'rU')
-for line in f:
-    cells = line.split(",")
-    name_input.append(cells[0])
-    banner_input.append(cells[1])
-    #bd_input.append(cells[2])
-    #class_input.append(cells[3])
-f.close()
 dd = {}
 ban = {}
 master = {}
-writer = csv.writer(open('newcsv.csv', 'w', newline=''))
-for x in range (0, len(banner_input)):
-    name = name_input[x]
-    banner = banner_input[x]
-    dis_name = name
-    dis_banner = 0
-    if banner in ban:
-        dis_banner = ban[banner]
+writer = csv.writer(open('newcsv2.csv', 'w', newline=''))
+for x in range (0, 4000):
+    dis_banner = "%0.9d" % random.randint(0,999999)
+    dis_name = ''
+    if dis_banner in ban:
         for key, value in master.items():
             if dis_banner == value:
                 dis_name = key
     else:
         dis_name = get_full_name()
-        #Not necessary unless we really want to have totally unique names (runtime increases a lot)
+        #Not necessary unless we want totally unique names (runtime increases a lot).
         #if dis_name in dd.values():
         #    dis_name = get_full_name()
-        dd[name] = dis_name
-        dis_banner = "%0.9d" % random.randint(0,999999)
-        if dis_banner in ban.values():
-            dis_banner = "%0.9d" % random.randint(0,999999)
-        ban[banner] = dis_banner
+        dd[dis_name] = dis_name
+        ban[dis_banner] = dis_banner
     master[dis_name] = dis_banner
-    #writer.writerow([dis_name,dis_banner,bd_input[x],class_input[x]])
     writer.writerow([dis_name,dis_banner])
-
-print("\nAvailable discombobulated identities:")
-for keys,values in master.items():
-  print("%s: %s" % (keys,values))
-  print("\n")
-    
